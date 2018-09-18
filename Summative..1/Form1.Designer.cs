91de2f8c2608d245597784173ac0f77848d235df
ohmstar123 <ohmpate114@ed.amdsb.ca>
2018-09-18 14:41:21 -0400

    mid way

--- Summative..1/Form1.Designer.cs
+++ Summative..1/Form1.Designer.cs
@@ -28,13 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Image_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Image_button
+            // 
+            this.Image_button.BackgroundImage = global::Summative._1.Properties.Resources.lion_king_wd070;
+            this.Image_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Image_button.CausesValidation = false;
+            this.Image_button.FlatAppearance.BorderSize = 0;
+            this.Image_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Image_button.Location = new System.Drawing.Point(12, 0);
+            this.Image_button.Name = "Image_button";
+            this.Image_button.Size = new System.Drawing.Size(759, 452);
+            this.Image_button.TabIndex = 0;
+            this.Image_button.UseVisualStyleBackColor = true;
+            this.Image_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Image_button);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Image_button;
     }
 }
 
--- Summative..1/Form1.cs
+++ Summative..1/Form1.cs
@@ -16,5 +16,15 @@ namespace Summative._._1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
